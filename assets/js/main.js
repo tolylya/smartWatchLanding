94256762e0c1c046949afa842c3df6a7626c01f8
tolylya <tolylya220gmail.com>
2015-11-17 20:40:06 +0200

    Extend site

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+$(function(){
+  $('.slider').glide({
+    autoplay: 2500,
+    hoverpause: true, // set to false for nonstop rotate
+    arrowRightText: '&rarr;',
+    arrowLeftText: '&larr;'
+  });
+});
